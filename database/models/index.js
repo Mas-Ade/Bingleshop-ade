a5c0713c632e4f8954ade112bb1bcc8078a67bce
Mas-Ade <120234644+Mas-Ade@users.noreply.github.com>
2023-01-26 16:36:52 +0700

    finalisasi assosiation table

--- database/models/index.js
+++ database/models/index.js
@@ -11,6 +11,17 @@ const StatusOrder = require('./tb_status_orders')
 // import sequilize module
 const sequelize = require('./sequelize')
 
+StatusOrder.hasMan(User,{
+  foreignKey: 'id_user'
+})
+
+Order.hasMan(User,{
+  foreignKey: 'id_user'
+})
+
+Cart.hasMany(user,{
+  foreignKey: 'id_user'
+})
 
 Item.hasMany(User,{
   foreignKey: 'id_user',
@@ -18,6 +29,18 @@ Item.hasMany(User,{
 
 User.belongsTo(Item,{
     foreignKey: 'id_user',
+})
+
+Item.belongsTo(Cart,{
+    foreignKey: 'id_item',
+})
+
+Cart.belongsTo(Order,{
+    foreignKey: 'id_cart',
+})
+
+Order.belongsTo(StatusOrder,{
+    foreignKey: 'id_order',
 })
 
  // export module user,product dan sequelize
